f657469a90afbe2cbb4dabb47eb8ef85408a303a
DarePL <damian.szymczyk@gmail.com>
2023-03-07 21:06:08 +0100

    added new project

--- script/script.js
+++ script/script.js
@@ -14,8 +14,8 @@ function query(currency){
 function displayCurrency(api) {
 		$("#currencyTable").append(
 		"<tr><th>waluta: </th><th>"+api.currency+"</th></tr>"+
-		'<tr><th>Kupno:</th><td class="buy">'+api.rates[0].bid+' zł</td></tr>'+
-		'<tr><th>Sprzedaż:</th><td class="sell">'+api.rates[0].ask+" zł</td></tr>"+
-		"<tr><td>Data publikacji:</td><td>"+api.rates[0].effectiveDate+"</td>/tr>"
+		'<tr><th>kupno:</th><td class="buy">'+api.rates[0].bid+' zł</td></tr>'+
+		'<tr><th>sprzedaż:</th><td class="sell">'+api.rates[0].ask+" zł</td></tr>"+
+		"<tr><td>data publikacji:</td><td>"+api.rates[0].effectiveDate+"</td>/tr>"
 		);
 }
